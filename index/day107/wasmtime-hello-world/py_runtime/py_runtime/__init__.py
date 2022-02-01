5dcb511954389159080e1105b9864abe6e83a262
Jiaxiao Zhou <jiazho@microsoft.com>
2022-02-01 13:40:51 -0800

    added wasmtime-py wasi example

--- index/day107/wasmtime-hello-world/py_runtime/py_runtime/__init__.py
+++ index/day107/wasmtime-hello-world/py_runtime/py_runtime/__init__.py
@@ -1,20 +1,27 @@
 __version__ = '0.1.0'
+from wasmtime import Engine, Func, Store, Module, Linker, WasiConfig, Instance
+
 
 if __name__ == '__main__':
-    from wasmtime import Store, Module, Instance, Func, FuncType
+    
+    engine = Engine()
+    store = Store(engine)
+    module = Module.from_file(engine, "crates/wasi/target/wasm32-wasi/release/wasi.wasm")
+    module2 = Module.from_file(engine, "crates/wasi_lib/target/wasm32-wasi/debug/wasi_lib.wasm")
 
-    store = Store()
-    module = Module(store.engine, """
-    (module
-        (func $hello (import "" "hello"))
-        (func (export "run") (call $hello))
-    )
-    """)
-
-    def say_hello():
-        print("Hello from Python!")
-    hello = Func(store, FuncType([], []), say_hello)
-
-    instance = Instance(store, module, [hello])
-    run = instance.exports(store)["run"]
-    run(store)
+    linker = Linker(engine)
+    linker.define_wasi()
+    
+    
+    wasi = WasiConfig()
+    wasi.inherit_stdout()
+    store.set_wasi(wasi)
+    
+    linking = linker.instantiate(store, module)
+    run = linking.exports(store)["main"]
+    run(store, 0, 0)
+     
+    linking2 = linker.instantiate(store, module2)
+    run = linking2.exports(store)["print_increment_num"]
+    run(store, 5)
+    
--- index/day107/wasmtime-hello-world/src/main.rs
+++ index/day107/wasmtime-hello-world/src/main.rs
@@ -5,6 +5,7 @@ use wasmtime_wasi::{WasiCtxBuilder, WasiCtx};
 fn main() -> Result<()> {
     let engine = Engine::default();
     let module = Module::from_file(&engine, "crates/wasi_lib/target/wasm32-wasi/debug/wasi_lib.wasm")?;
+    let module2: Module = Module::from_file(&engine, "crates/wasi/target/wasm32-wasi/release/wasi.wasm")?;
     let mut linker = Linker::new(&engine);
 
     let wasi_ctx = WasiCtxBuilder::new().inherit_stdio().inherit_args()?.build();
@@ -12,36 +13,21 @@ fn main() -> Result<()> {
     wasmtime_wasi::add_to_linker(&mut linker, |cxt: &mut Context| &mut cxt.wasi)?;
     let mut store = Store::new(&engine, ctx);
 
+
     let instance = linker.instantiate(&mut store, &module)?;
     instance.get_typed_func::<(), (), _>(&mut store, "print_hello")?.call(&mut store, ())?;
     instance.get_typed_func::<i32, (), _>(&mut store, "print_increment_num")?.call(&mut store, 5)?;
     // instance.get_typed_func::<&str, (), _>(&mut store, "print_str")?.call(&mut store, "hello asdfasdfasdf")?; 
     // -> the trait `WasmTy` is not implemented for `&str`
 
+    let instance = linker.instantiate(&mut store, &module2)?;
+    instance.get_typed_func::<(i32, i32), i32, _>(&mut store, "main")?.call(&mut store, (0, 0))?;
+
     Ok(())
 }
-
-// fn main() -> Result<()> {
-//     let engine = Engine::default();
-//     let module = Module::from_file(&engine, "crates/wasi/target/wasm32-wasi/release/wasi.wasm")?;
-//     let mut linker = Linker::new(&engine);
-
-//     let wasi_ctx = WasiCtxBuilder::new().inherit_stdio().inherit_args()?.build();
-//     let ctx = Context { wasi: wasi_ctx, runtime_data: 100 };
-//     wasmtime_wasi::add_to_linker(&mut linker, |cxt: &mut Context| &mut cxt.wasi)?;
-//     let mut store = Store::new(&engine, ctx);
-
-//     // linker.module(&mut store, "wasi", &module)?;
-//     // linker.get(&mut store, "wasi", "add_and_print");
 //     linker.get_default(&mut store, "")?
 //           .typed::<(), (), _>(&store)?
 //           .call(&mut store, ())?;
-//     // let instance = linker.instantiate(&mut store, &module)?;
-//     // instance.get_typed_func::<(), (), _>(&mut store, "main")?.call(&mut store, ())?;
-
-//     Ok(())
-// }
-
 
 // fn main() -> Result<()> {
 //     let engine = Engine::default();
@@ -65,10 +51,6 @@ fn main() -> Result<()> {
 //     To fix this error, we will need to link to wasmtime_wasi
 //     */
 // }
-
-fn host_hello() {
-
-}
 
 struct Context {
     pub wasi: WasiCtx,
