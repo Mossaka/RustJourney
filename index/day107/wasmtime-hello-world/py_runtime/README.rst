0d7350c723e7598719bf95392156a95cf35c5fe3
Jiaxiao Zhou <jiazho@microsoft.com>
2022-02-01 12:48:42 -0800

    added a wasmtime-py example

---
+++ index/day107/wasmtime-hello-world/py_runtime/README.rst
@@ -0,0 +1,4 @@
+## How to Run:
+
+1. `poetry install`
+2. `poetry run python py_runtime/__init__.py ` 
---
+++ index/day107/wasmtime-hello-world/py_runtime/py_runtime/__init__.py
@@ -0,0 +1,20 @@
+__version__ = '0.1.0'
+
+if __name__ == '__main__':
+    from wasmtime import Store, Module, Instance, Func, FuncType
+
+    store = Store()
+    module = Module(store.engine, """
+    (module
+        (func $hello (import "" "hello"))
+        (func (export "run") (call $hello))
+    )
+    """)
+
+    def say_hello():
+        print("Hello from Python!")
+    hello = Func(store, FuncType([], []), say_hello)
+
+    instance = Instance(store, module, [hello])
+    run = instance.exports(store)["run"]
+    run(store)
